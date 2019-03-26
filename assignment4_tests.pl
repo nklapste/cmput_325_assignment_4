cd8b80aa2a6def59b7cc415e8d8c90eda3260d6d
Nathan Klapstein <nklapste@ualberta.ca>
2019-03-26 08:34:08 -0600

    adding tests for `count`

--- assignment4_tests.pl
+++ assignment4_tests.pl
@@ -3,7 +3,14 @@
 :- begin_tests(assignment4).
 
 % question 1
-% test(count, all(X=[3,4,5,6,7])) :- count(3, 7, X).
+test(count, all(X=[3,4,5,6,7])) :- count(3, 7, X).
+test(count, all(X=[3])) :- count(3, 3, X).
+test(count, all(X=[3,2,1,0])) :- count(3, 0, X).
+test(count, all(X=[0])) :- count(0, 0, X).
+test(count, all(X=[0,-1,-2,-3])) :- count(0, -3, X).
+test(count, all(X=[-3,-2,-1,0])) :- count(-3, 0, X).
+test(count, all(X=[-3,-2,-1,0,1,2,3])) :- count(-3, 3, X).
+test(count, all(X=[3,2,1,0,-1,-2,-3])) :- count(3, -3, X).
 % question 2
 
 :- end_tests(assignment4).
