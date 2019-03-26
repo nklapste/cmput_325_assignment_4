0084108752f5b6c336b21f7685e353ef2b3ad193
Nathan Klapstein <nklapste@ualberta.ca>
2019-03-26 08:45:04 -0600

    adding tests for `fourSquares`

--- assignment4_tests.pl
+++ assignment4_tests.pl
@@ -11,6 +11,11 @@ test(count, all(X=[0,-1,-2,-3])) :- count(0, -3, X).
 test(count, all(X=[-3,-2,-1,0])) :- count(-3, 0, X).
 test(count, all(X=[-3,-2,-1,0,1,2,3])) :- count(-3, 3, X).
 test(count, all(X=[3,2,1,0,-1,-2,-3])) :- count(3, -3, X).
+
+test(fourSquares, all(S=[[0,0,0,1], [0,0,1,0], [0,1,0,0], [1,0,0,0]])) :- fourSquares(1, S).
+test(fourSquares, all(S=[[0,0,1,1],[0,1,0,1],[0,1,1,0],[1,0,0,1],[1,0,1,0],[1,1,0,0]])) :- fourSquares(2, S).
+test(fourSquares, all(S=[[0,0,0,0]])) :- fourSquares(0, S).
+test(fourSquares, fail) :- fourSquares(-1, _).
 % question 2
 
 :- end_tests(assignment4).
